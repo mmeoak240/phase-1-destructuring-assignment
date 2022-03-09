daa2e8cbcfefb74073c18c6b79fa3d33bd8be074
Michael Meoak <mmeoak240@gmail.com>
2022-03-09 10:26:55 -0700

    commit tree c03f9cc84309e01d821dcab4f591d65211731790

--- index.js
+++ index.js
@@ -25,6 +25,46 @@ const nestedMuppet = {
   nestedPartner: 'Miss Piggy'
 };
 
+const farmNoises = 'cow horse sheep pig chicken';
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ')
+
+const farmNames = 'cow horse sheep pig chicken'
+const [bessie, ,dolly, babe, little] = farmAnimals.split(' ')
+
+const farmColors = 'cow horse sheep pig chicken';
+const [blackAndWhite, ,black, pink, ] = farmAnimals.split(' ')
+
+const [red, orange, yellow, green, blue, indigo, violet] = colors
+const[r, o, y, g, b, , v] = colors
+const [, , , , , indg, ] = colors
+
+// const muppet = {
+//   muppetName: 'Miss Piggy',
+//   color: 'pink',
+//   song: 'Never Before, Never Again',
+//   job: 'Cast member of The Muppet Show',
+//   partner: 'Kermit'
+// };
+
+// const nestedMuppet = {
+//   nestedName: 'Kermit',
+//   nestedColor: 'green',
+//   album: {
+//     theMuppetMovie: {
+//       song1: 'Rainbow Connection',
+//       song2: 'Moving Right Along',
+//       song3: 'Never Before, Never Again',
+//       song4: 'I Hope That Something Better Comes Along',
+//     },
+//   },
+//   nestedJob: 'Host of The Muppet Show',
+//   nestedPartner: 'Miss Piggy'
+// };
+
+const {muppetName, color, song, job, partner} = muppet
+const {song2, song4} = nestedMuppet.album.theMuppetMovie
+const {nestedJob, nestedPartner} = nestedMuppet
+console.log(song2)
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
